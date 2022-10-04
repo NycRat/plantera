7e50a261fa176bae2d02a996591d3167b7b8b3c8
Eric Xiao <ericxiao0000@gmail.com>
2022-10-04 08:38:56 -0700

    Changed structure of database, uuid for plants

--- backend/src/models/plant.rs
+++ backend/src/models/plant.rs
@@ -1,5 +1,17 @@
+use rocket::serde::Deserialize;
+
+// idk why this is here im not even using
 #[derive(Debug, PartialEq, Eq)]
 pub struct Plant {
-    pub id: i32,
+    pub id: String,
+    pub user: String,
     pub name: String,
+    pub watering_interval: u64,
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct NewPlantRequest {
+    pub name: String,
+    pub watering_interval: u64,
 }
--- backend/src/routes/plant_routes.rs
+++ backend/src/routes/plant_routes.rs
@@ -1,3 +1,4 @@
+use crate::models::plant::NewPlantRequest;
 use crate::utils::is_authorized;
 use mysql::prelude::*;
 use mysql::*;
@@ -18,16 +19,16 @@ pub async fn get_plant_list(
 
     let plant_ids = conn.query_first::<String, &String>(&query_string).unwrap();
     if let Some(some_plant_ids) = plant_ids {
-        let plant_id_arr = rocket::serde::json::from_str::<Vec<u32>>(&some_plant_ids).unwrap();
+        let plant_id_arr = rocket::serde::json::from_str::<Vec<String>>(&some_plant_ids).unwrap();
         if plant_id_arr.len() == 0 {
             return (Status::Ok, "[]".into());
         }
 
         let mut where_str = "".to_owned();
         for plant in plant_id_arr {
-            where_str += "ID = ";
+            where_str += "id = \"";
             where_str += &plant.to_string();
-            where_str += " OR ";
+            where_str += "\" OR ";
         }
 
         let where_str = &where_str[0..where_str.len() - 4];
@@ -51,10 +52,19 @@ pub async fn post_plant_new(
     conn_mutex: &State<Mutex<PooledConn>>,
     auth: Authentication,
 ) -> Status {
-    let name = data.open(1.kilobytes()).into_string().await.unwrap();
-    let trimmed_name = name.trim();
-    if trimmed_name.len() == 0 {
-        return Status::BadRequest;
+    let req = data.open(1.kilobytes()).into_string().await.unwrap();
+    let plant_info: NewPlantRequest;
+    match rocket::serde::json::from_str::<NewPlantRequest>(&req) {
+        Ok(req) => {
+            plant_info = req;
+            if plant_info.name.len() == 0 {
+                return Status::BadRequest;
+            }
+        }
+        Err(err) => {
+            println!("{:?}", err);
+            return Status::BadRequest;
+        }
     }
 
     let mut conn = conn_mutex.lock().await;
@@ -67,15 +77,27 @@ pub async fn post_plant_new(
                 return Status::Unauthorized;
             }
 
-            // TODO - actually do the plant ID
-            let query_string = format!(
-                "UPDATE users SET plants = JSON_ARRAY_APPEND(plants, \"$\", JSON_LENGTH(plants)) WHERE username = \"{}\"",
+            let get_user_id = format!(
+                "(SELECT username from users WHERE username = \"{}\")",
                 login_info.user_id
             );
 
-            conn.query_drop(query_string).unwrap();
-            let two = format!("INSERT INTO plants (name) VALUES (\"{}\")", &trimmed_name);
-            conn.query_drop(two).unwrap();
+            let plant_id = uuid::Uuid::new_v4().to_string();
+            let insert_plant_table_str = format!("INSERT INTO plants (id, name, user, watering_interval) VALUES (\"{}\", \"{}\", {}, {})", 
+                                                 &plant_id,
+                                                 &plant_info.name,
+                                                 &get_user_id,
+                                                 &plant_info.watering_interval
+                                                 );
+            conn.query_drop(insert_plant_table_str).unwrap();
+
+            // todo!("REPLACE JSON LEN WITH THE UUID OF PLANT");
+            let insert_user_plants_str = format!(
+                "UPDATE users SET plants = JSON_ARRAY_APPEND(plants, \"$\", \"{}\") WHERE username = \"{}\"",
+                plant_id,
+                login_info.user_id
+            );
+            conn.query_drop(insert_user_plants_str).unwrap();
             return Status::Created;
         }
         _ => {
