e738e36b70449e04917cf02dff38436176803211
Eric Xiao <ericxiao0000@gmail.com>
2022-10-04 17:25:27 -0700

    Frontend initial commit

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <a href="/">Plantera</a>
+        <a href="/login">Login</a>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/web/src/pages/home/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = (): JSX.Element => {
+  return <div></div>
+}
+
+export default HomePage;
