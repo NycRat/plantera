b7675a271ddbb06d5386cc13f8a9596fed2b8127
Eric Xiao <ericxiao0000@gmail.com>
2022-11-07 17:40:03 -0800

    better new plant "menu"

--- frontend/web/src/pages/PlantListPage.tsx
+++ frontend/web/src/pages/PlantListPage.tsx
@@ -34,7 +34,7 @@ const PlantListPage = (): JSX.Element => {
     if (plantImageRef.current.files !== null) {
       const image = plantImageRef.current.files[0];
       if (image !== undefined) {
-        reader.readAsDataURL(plantImageRef.current.files[0]);
+        reader.readAsDataURL(image);
       }
     }
   };
@@ -42,15 +42,19 @@ const PlantListPage = (): JSX.Element => {
   const handleCreatePlant = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (plantImageRef.current !== null) {
+      plantImageRef.current.value = "";
+    }
+
     let newPlant = defaultNewPlant;
     if (preview !== "") {
       newPlant.image = preview;
     }
+    const previewTemp = preview;
+    setPreview("");
     dispatch(addPlant(newPlant));
     await apiPostNewPlant(newPlant);
-    await apiPostPlantImage(preview, 0);
-
-    setPreview("");
+    await apiPostPlantImage(previewTemp, 0);
   };
 
   return (
@@ -61,6 +65,55 @@ const PlantListPage = (): JSX.Element => {
         border-4 border-black rounded-lg"
       >
         <h1 className="m-0 mb-4 text-6xl">Plants</h1>
+
+        <div
+          onClick={() => {
+            /* navigate(`/plant?index=${i}`); */
+          }}
+          className="relative inline-block 
+            w-[calc(90vw-60px)]
+            h-[calc(90vw-60px)] 
+            sm:w-[calc(90vw/2-90px/2)]
+            sm:h-[calc(90vw/2-90px/2)] 
+            md:w-[calc(90vw/3-100px/3)]
+            md:h-[calc(90vw/3-100px/3)] 
+            lg:sm:w-[calc(90vw/4-100px/4)]
+            lg:sm:h-[calc(90vw/4-100px/4)] 
+            rounded-lg mx-[5px] my-[2px] bg-color-light-1 transition-shadow
+            hover:shadow-2xl cursor-pointer border border-black
+            overflow-hidden whitespace-nowrap"
+        >
+          <input
+            type={"file"}
+            ref={plantImageRef}
+            onChange={handleUpdatePlantImage}
+            className="bg-white text-black z-20 absolute
+              left-1/2
+              -translate-x-1/2
+            "
+          />
+          <form onSubmit={handleCreatePlant}>
+            <input
+              type={"submit"}
+              value={"NEW PLANT"}
+              className="px-2 py-1 rounded-full absolute duration-100
+              bg-color-dark-2 hover:bg-color-dark-1 z-20
+              bottom-1 left-1/2 -translate-x-1/2
+              "
+            />
+          </form>
+          <img
+            src={preview}
+            alt="New Plant"
+            className="left-0 top-0 absolute w-[100%] h-[100%] object-cover
+                rounded-lg z-10"
+          />
+          <div
+            className="absolute bottom-0 text-3xl text-center
+              w-[100%] bg-[#000000a0] h-10 z-10"
+          />
+        </div>
+
         {plantList.map((plant, i) => (
           <div
             key={i}
@@ -97,21 +150,6 @@ const PlantListPage = (): JSX.Element => {
           </div>
         ))}
       </div>
-      <input
-        type={"file"}
-        ref={plantImageRef}
-        className="bg-white text-black"
-        onChange={handleUpdatePlantImage}
-      />
-      <form onSubmit={handleCreatePlant}>
-        <input
-          type={"submit"}
-          value={"NEW PLANT"}
-          className="px-2 py-1 rounded-full m-2
-          duration-100 bg-color-dark-2 hover:bg-color-dark-1"
-        />
-      </form>
-      <img src={preview} alt={"haha"} />
     </div>
   );
 };
